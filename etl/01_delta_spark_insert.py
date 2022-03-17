2d523b8e44b32e7e39facf714d0ea49c662f69d0
educostalui <educosta.lui@gmail.com>
2022-03-17 17:19:18 -0300

    ajuste no nome do bucket

--- etl/01_delta_spark_insert.py
+++ etl/01_delta_spark_insert.py
@@ -18,7 +18,7 @@ enem = (
     .option("inferSchema", True)
     .option("header", True)
     .option("delimiter", ";")
-    .load("s3://datalake-edu-igti-edc/raw-data/enem")
+    .load("s3://datalake-educosta-igti-edc/raw-data/enem")
 )
 
 # Escreve a tabela em staging em formato delta
